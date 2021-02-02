fc188dc4fa9bb5ae077829454d1866ccf4d41cfb
ghervouin <54936763+ghervouin@users.noreply.github.com>
2021-02-02 21:21:49 +0100

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,11 +1,11 @@
+var fs = require('fs');
+fs.readFile( __dirname + '/rdu-weather-history.json', function (err, data) {
+  if (err) {
+    throw err; 
+  }
+});
+
 module.exports = async function (context, req) {
-  var fs = require('fs');
-  fs.readFile( __dirname + '/rdu-weather-history.json', function (err, data) {
-    if (err) {
-      throw err; 
-    }
-  });
-  
   context.res = {
     body: {
       text: data.toString()

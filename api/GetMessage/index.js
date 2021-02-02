02b9f3f1b49f64f340693577556b1d3827f51850
ghervouin <54936763+ghervouin@users.noreply.github.com>
2021-02-02 22:11:58 +0100

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,8 +1,9 @@
-module.exports = async function (context, req) {
-    var message = "[{fogground: null}" \ 
+var message = "[{fogground: null}" \ 
     ",{fogground: null}" \ 
     ",{fogground: null}" \ 
     "]";
+
+module.exports = async function (context, req) {
     context.res = {
       body: { 
         text: "Hello from the API 2" 

4cd7ab8c918c12634ed69295106784217ef16821
ghervouin <54936763+ghervouin@users.noreply.github.com>
2021-02-02 22:55:27 +0100

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -2,18 +2,12 @@ var fs = require('fs');
 
 module.exports = async function (context, req) {
     var path = __dirname + '//weather.json';
-    fs.readFile(path, 'utf8', function (err, data) {
-        if (err) {
-            context.log.error(err);
-            context.done(err);
-        }
-
-        context.log(result.name);
+    
         context.res = {
           body: { 
             text: path 
           }
         };
-    });
+    
     
   };

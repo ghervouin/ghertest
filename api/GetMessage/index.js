330c0dc8360554ba4f48bdd296dd237d6b2beebe
ghervouin <54936763+ghervouin@users.noreply.github.com>
2021-02-02 22:07:05 +0100

    Update index.js

--- api/GetMessage/index.js
+++ api/GetMessage/index.js
@@ -1,7 +1,11 @@
 module.exports = async function (context, req) {
+    const txt = "[{\"fogground\": null, \"snowfall\": 0.0, \"dust\": null, \"snowdepth\": 0.0}" \ 
+    ",{\"fogground\": null, \"snowfall\": 0.0, \"dust\": null, \"snowdepth\": 0.0}" \ 
+    ",{\"fogground\": null, \"snowfall\": 0.0, \"dust\": null, \"snowdepth\": 0.0}" \ 
+    "]";
     context.res = {
       body: { 
-        text: "Hello from the API" 
+        text: "Hello from the API 2" 
       }
     };
   };

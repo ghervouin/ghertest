a4410a171d4af675606a1093e34d35aaf9aa187b
ghervouin <54936763+ghervouin@users.noreply.github.com>
2021-03-10 15:49:03 +0100

    Update index.js

--- api/GetShort/index.js
+++ api/GetShort/index.js
@@ -12,7 +12,7 @@ module.exports = async function (context, req) {
 
     context.res = {
       headers: {
-        'Content-Type': 'application/json; charset=utf-8',
+        'Content-Type': 'application/json',
         'Cache-Control': 'private, no-cache, no-store, must-revalidate',
         'Expires': '-1'
       },
